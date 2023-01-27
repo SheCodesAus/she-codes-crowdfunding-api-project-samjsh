e05735e022d68ca622a92e1a99de3a25e1f0d5a6
samjsh <sjaneshearman@gmail.com>
2023-01-27 14:30:51 +0800

    Status Codes
    Able to get the status codes working.

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -12,6 +12,6 @@ class ProjectSerializer(serializers.Serializer):
 	is_open = serializers.BooleanField()
 	date_created = serializers.DateTimeField()
 	owner = serializers.CharField(max_length=200)
-
-def create(self, validated_data):
-    return Project.objects.create(**validated_data)
+	
+	def create(self, validated_data):
+		return Project.objects.create(**validated_data)
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project
 from .serializers import ProjectSerializer
+from django.http import Http404
+from rest_framework import status
 
-class ProjectDetail(APIView): 
+#Create your views here
+class ProjectDetail(APIView):
 
-    def get_object(self, pk): 
-        return Project.objects.get(pk=pk) 
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk) 
+        except Project.DoesNotExist:
+            raise Http404
 
-    def get(self, request, pk): 
+    def get(self, request, pk):
         project = self.get_object(pk) 
         serializer = ProjectSerializer(project) 
         return Response(serializer.data) 
@@ -23,20 +27,20 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
     
-    #below block has been added
     def post(self, request):
+        print("hello")
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
+            print("valid")
             serializer.save()
-            return Response(serializer.data)
-
-#request body (this block has also been added)
-{
-	"title": "Project one",
-	"description": "The first project.",
-	"goal": 150,
-	"image": "https://via.placeholder.com/300.jpg",
-	"is_open": True,
-	"date_created": "2020-03-20T14:28:23.382748Z",
-	"owner": "Real Creator"
-}
+            print("saved!")
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+                )
+        print("invalid")
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        
